fed2897400686f600f61f786f54b15aa4eea46a6
Filipp Kramer <krame490@gmail.com>
2019-12-23 23:26:44 -0600

    add REPL and main.go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "os/user"
+  "interpreter/repl"
+)
 
 func main() {
-  fmt.Println("Hello world")
+  user, err := user.Current()
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Printf("Hello %s, Feel free to type in commands\n", user.Username)
+  repl.Start(os.Stdin, os.Stdout)
 }
