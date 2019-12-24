9424c7adf6e52f83db9009de2e35e8ccaa245b72
Filipp Kramer <krame490@gmail.com>
2019-12-24 11:04:59 -0600

    add ast structs for let statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,24 @@
 package ast
 
+import "interpreter/token"
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
 type Node interface {
 	TokenLiteral() string
 }
